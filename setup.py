dc40b538d6e4b90412fc1189d455740a65a76d47
claudio-walser <walsercl@gmx.ch>
2017-02-06 22:08:45 +0100

    Initial Version

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(
+    name='cli',
+    version='0.0.1',
+    description='Helper for Python CLI Scripts',
+    author='Claudio Walser',
+    author_email='claudio.walser@srf.ch',
+    url='https://github.com/claudio-walser/python-cli',
+    packages=[
+        'Cli'
+    ]
+)
