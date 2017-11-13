7e1dc2f1dad850cda295104b6b6f0c478aec5ecd
Claudio Walser <claudio.walser@srf.ch>
2017-11-13 20:26:17 +0100

    renaming

--- setup.py
+++ setup.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+def read(fpath):
+    with open(fpath, 'r') as f:
+        return f.read()
+
+
+def version(fpath):
+    return read(fpath).strip()
+
 
 setup(
-    name='simple-cli',
-    version='0.0.2',
+    name='simplecli',
+    version='0.0.1',
     description='Helper for Python CLI Scripts',
     author='Claudio Walser',
     author_email='claudio.walser@srf.ch',
-    url='https://github.com/claudio-walser/python-cli',
-    packages=[
-        'Cli'
+    url='https://github.com/claudio-walser/python-simplecli',
+    packages=find_packages(),
+    license='Apache License',
+    keywords=['cli'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Utilities'
     ]
 )
+
